4dddc8247bb25ed044e49b2ca773b040f4f79267
mbutto26 <102936996+mbutto26@users.noreply.github.com>
2023-10-12 16:40:25 -0700

    Update Class 1 Exercises.js
    first question

--- Class 1 Exercises.js
+++ Class 1 Exercises.js
@@ -6,18 +6,16 @@
 // What is the area for each of these pizzas?
 // (radius would be the listed size - i.e. 13" - divided by 2)
 
-
+let pizzaSmallArea = `16.99 % 13`;
+let pizzaLargeArea = `19.99 % 17`;
 // 2. What is the cost per square inch of each pizza?
-
 
 // 3. Using the Math object, put together a code snippet
 // that allows you to draw a random card with a value
 // between 1 and 13 (assume ace is 1, jack is 11…)
 
-
 // 4. Draw 3 cards and use Math to determine the highest
 // card
-
 
 /**
  * ADDRESS LINE
@@ -28,15 +26,13 @@
 // this information to create a formatted address block
 // that could be printed onto an envelope.
 
-
 // 2. You are given a string in this format:
 // firstName lastName(assume no spaces in either)
 // streetAddress
 // city, state zip(could be spaces in city and state)
-// 
+//
 // Write code that is able to extract the first name from this string into a variable.
 // Hint: use indexOf, slice, and / or substring
-
 
 /**
  * FIND THE MIDDLE DATE
